50252bddd5f9ad23eeebacc94f6466cb66e09546
jack <jackrmcshane@gmail.com>
2022-03-22 15:06:28 -0400

    adding basic tmux config

--- scripts/debian_install.sh
+++ scripts/debian_install.sh
@@ -17,7 +17,7 @@ sudo apt install -y neovim zsh tmux zathura
 
 
 echo '[*] Installing programming languages...'
-sudo apt install -y python3 python3-pip python3-venv default-jdk
+sudo apt install -y python python3 python3-pip python3-venv default-jdk
 
 
 echo '[*] Setting up Neovim...'
